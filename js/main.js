78bd0912f65c64d222846ea96449e14100420fb7
hjung17 <hjung17@ucsc.edu>
2018-08-15 12:50:04 -0700

    framework with phaser

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Gloabl vars
+var game;
+var platforms, player, stars, diamonds;
+var score = 0;
+var scoreText;
+
+window.onload = function() {
+	// Define game
+	game = new Phaser.game(700, 800, Phaser.AUTO, 'game');
+
+	// Define states
+	// Start state
+}
